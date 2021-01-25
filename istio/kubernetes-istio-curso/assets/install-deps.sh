da5bdfaee0cc97f0e802d9d201ee5a24e0697c07
Anderson Santos <adsantos@gmail.com>
2021-01-25 15:53:28 -0300

    fix images

--- istio/kubernetes-istio-curso/assets/install-deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt -y update
-apt -y install httpie
-wget https://github.com/wercker/stern/releases/download/1.11.0/stern_linux_amd64
-chmod +x stern_linux_amd64
-mv stern_linux_amd64 /usr/local/bin/stern
